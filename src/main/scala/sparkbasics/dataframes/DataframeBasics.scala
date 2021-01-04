2e3dd64d51e6f72514a033ba67b8eefe65c1c013
mrayandutta21 <mrayandutta21@gmail.com>
2021-01-04 09:31:36 +0000

    Made changes for spark session import

--- src/main/scala/sparkbasics/dataframes/DataframeBasics.scala
+++ src/main/scala/sparkbasics/dataframes/DataframeBasics.scala
@@ -1,17 +1,18 @@
 package sparkbasics.dataframes
-/*
-import org.apache.spark.SparkSession
-val spark:SparkSession = SparkSession.builder()
-      .master("local[1]")
-      .appName("SparkByExamples.com")
-      .getOrCreate()
-      * 
-      */
+
+import org.apache.spark.sql.SparkSession
+
 object DataframeBasics {
-  
-val columns = Seq("language","users_count")
-val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
-//val distData = sc.parallelize(data)
-//val df = data.toDF(columns:_*)
-//df.show()
+
+  val spark: SparkSession = SparkSession.builder()
+    .master("local[1]")
+    .appName("SparkByExamples.com")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val columns = Seq("language", "users_count")
+  val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
+  val df = data.toDF(columns: _*)
+  df.show()
 }
